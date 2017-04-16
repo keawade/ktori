09539d3f18aed70e0fb615c580292a939f9e73fd
Keith Wade <keawade@gmail.com>
2017-04-16 10:38:50 -0500

    Tweak details

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,6 +1,7 @@
 <template>
   <div class='ktori-details'>
-    <h1>Keith and Victoria</h1>
+    <div class='ui hidden divider'></div>
+    <h1 class='ktori-title'>Keith and Victoria</h1>
     <h3>Sunday, September 3, 2017 - Westminster, CO</h3>
     <div class='ui hidden divider'></div>
     <div>Thank you for taking the time to check out our website! We are excited to see all of you at our wedding! - Keith and Tori</div>
@@ -25,5 +26,9 @@ export default {
 <style lang='scss' scoped>
   .ktori-details {
     text-align: center;
+
+    .ktori-title {
+      font-size: 2.9rem;
+    }
   }
 </style>
