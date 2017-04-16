c43d4c7bf3352e49998571856de2c77d7fa98aea
Keith Wade <keawade@gmail.com>
2017-04-16 10:34:35 -0500

    Update layout and add text

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -2,10 +2,16 @@
   <div class='ktori-details'>
     <h1>Keith and Victoria</h1>
     <h3>Sunday, September 3, 2017 - Westminster, CO</h3>
-    <img class='ui rounded image' src='/static/ktori_26.jpg'>
+    <div class='ui hidden divider'></div>
+    <div>Thank you for taking the time to check out our website! We are excited to see all of you at our wedding! - Keith and Tori</div>
+    <div class='ui hidden divider'></div>
+    <div class='ui hidden divider'></div>
     <h2>Butterfly Pavilion</h2>
     <h4>6252 W 104th Ave, Westminster, CO 80020</h4>
+    <div class='ui hidden divider'></div>
     <a class='ui button' href='http://ktori.rsvpify.com' target='_blank'>RSVP</a>
+    <div class='ui hidden divider'></div>
+    <img class='ui rounded image' src='/static/ktori_26.jpg'>
   </div>
 </template>
 
@@ -19,8 +25,5 @@ export default {
 <style lang='scss' scoped>
   .ktori-details {
     text-align: center;
-  }
-  img {
-    padding-bottom: 1rem;
   }
 </style>
--- src/components/Registry.vue
+++ src/components/Registry.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class='ktori-registry'>
+    <div>Your presence at our wedding is the greatest gift of all. However, if you wish to honor us with a gift, we have registered lists with Amazon and Bed Bath & Beyond.</div>
     <h2>
       <a href='https://www.amazon.com/wedding/victoria-leddy-keith-wade-westminster-september-2017/registry/LEJ15O76FV43'>Amazon Wedding Registry</a>
     </h2>
@@ -17,4 +18,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='scss' scoped>
+  .ktori-registry {
+    text-align: center;
+  }
 </style>
