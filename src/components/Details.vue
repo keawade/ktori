0ea96ca73cab4af95822ec33eedb104e5f65b998
Keith Wade <keawade@gmail.com>
2017-04-16 09:48:34 -0500

    Update details

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,8 +1,10 @@
 <template>
   <div class='ktori-details'>
     <h1>Keith and Victoria</h1>
-    <h3>September 3, 2017 - Westminster, CO</h3>
+    <h3>Sunday, September 3, 2017 - Westminster, CO</h3>
     <img class='ui rounded image' src='/static/ktori_26.jpg'>
+    <h2>Butterfly Pavilion</h2>
+    <h4>6252 W 104th Ave, Westminster, CO 80020</h4>
     <a class='ui button' href='http://ktori.rsvpify.com' target='_blank'>RSVP</a>
   </div>
 </template>
